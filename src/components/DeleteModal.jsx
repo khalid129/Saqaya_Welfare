3e9c82a81a6e4cfeebcd50e3e59ff6be99885e85
Muhammad Khalid <58352840+khalid129@users.noreply.github.com>
2021-10-31 13:36:08 +0500

    delete modal button alignment

--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -26,24 +26,26 @@ const DeleteModal=(props)=> {
         <ModalHeader>آمدن</ModalHeader>
         <ModalBody>
           <h1>کیا آپ واقعی اسے حذف کرنا چاہتے ہیں؟</h1>
-          {/*<Button
+          <div style={{display:"flex", alignItems:"center",justifyContent:"left"}}>
+          <Button
             type="submit"
             value="submit"
             style={{
               backgroundColor: "#90DAF2",
               color: "black",
               borderColor: "#90DAF2",
-              marginTop: "10px",
+              marginRight: "10px",
             }}
           >
             Delete
-        </Button>*/}
+        </Button>
           <Button
             onClick={() => cancel()}
             style={{ backgroundColor: "red" }}
           >
             cancel
           </Button>
+          </div>
         </ModalBody>
       </Modal>
     </>
